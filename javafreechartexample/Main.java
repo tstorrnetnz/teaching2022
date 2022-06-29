0b44618b04920aee9c162f464d5d3c6043109822
tstorrnetnz <tstorr@netnz.school.nz>
2022-06-30 10:02:48 +1200

    Add files via upload

---
+++ javafreechartexample/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javafreechartexample;
+
+/**
+ *
+ * @author trevor
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+            Javafreechartexample demo = new Javafreechartexample("Comparison", "Which operating system are you using?");
+            demo.pack();
+            demo.setVisible(true);
+        }
+    
+}
